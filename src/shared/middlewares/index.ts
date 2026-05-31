691fe4db8cd7961a5909941e38eccfa22fdfb708
itallo vidal <itallovidalll@gmail.com>
2026-05-31 18:25:08 -0300

    feat(auth): add admin middleware for route authorization

--- src/shared/middlewares/index.ts
+++ src/shared/middlewares/index.ts
@@ -1,1 +1,23 @@
-export {};
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export async function adminMiddleware(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const adminId = request.headers["admin-id"] as string | undefined;
+
+  if (!adminId) {
+    reply.code(401);
+
+    reply.send({
+      status: 401,
+      error: {
+        message: "Autenticação necessária.",
+        code: "UNAUTHORIZED",
+      },
+      data: null,
+    });
+
+    return;
+  }
+}
