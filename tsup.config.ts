6887c12886e24157246386c16d7ab34d1a640460
itallo vidal <itallovidalll@gmail.com>
2026-06-14 22:56:32 -0300

    fix(deploy): switch tsup output to CJS to support require in dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
-  format: "esm",
+  format: "cjs",
   clean: true,
   dts: true,
   sourcemap: true,
